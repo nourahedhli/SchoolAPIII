b12c2855b46e3ca9674ff32f979bd5a6f289ba89
ahedhli21 <55544100+nourahedhli@users.noreply.github.com>
2021-04-27 12:36:27 -0400

    adding a new recrod

--- SchoolAPIII/Migrations/20210427163529_AddingNewUser.Designer.cs
+++ SchoolAPIII/Migrations/20210427163529_AddingNewUser.Designer.cs
@@ -4,14 +4,16 @@ using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace SchoolAPIII.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20210427163529_AddingNewUser")]
+    partial class AddingNewUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SchoolAPIII/Migrations/20210427163529_AddingNewUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolAPIII.Migrations
+{
+    public partial class AddingNewUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "OrganizationId", "UserName" },
+                values: new object[] { new Guid("021ca3c1-0deb-4afd-ae94-2159a8479812"), new Guid("3d490a70-94ce-4d15-9494-5248280c2ce3"), "nouraHedhli" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: new Guid("021ca3c1-0deb-4afd-ae94-2159a8479812"));
+        }
+    }
+}
