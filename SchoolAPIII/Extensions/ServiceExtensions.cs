a4522661d29a0328750b90b5707012ea620bbbfb
ahedhli21 <55544100+nourahedhli@users.noreply.github.com>
2021-04-27 00:54:42 -0400

    Add project files.

---
+++ SchoolAPIII/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿
+using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SchoolAPIII.Extensions
+{
+    public static class ServiceExtensions
+    {
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
